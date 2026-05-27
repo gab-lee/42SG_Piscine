b428319e4c2e8d5faaf9bd4a3a4b79138508b1ac
Gab <53164692+gab-lee@users.noreply.github.com>
2026-05-27 11:22:56 +0800

    updated but stuck

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void ft_print_comb2(void){
+	int n = 0
+	while(n<=99){
+	char tensP = n/10 + '0'
+	char onesP = n %10 + '0'
+	if (tensP == onesP){
+	n++;
+	}	
+	else{
+	n++;
+	}
+
+}
+
+
+}
