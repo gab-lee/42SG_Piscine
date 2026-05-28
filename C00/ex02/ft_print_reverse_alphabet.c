9134cfaaaa3347323be1ea28bf719a049f43484e
Gabriel Lee <gabrlee@c2r3s4.42singapore.sg>
2026-05-28 15:52:46 +0800

    norm-ed ex02

--- C00/ex02/ft_print_reverse_alphabet.c
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -1,10 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 15:45:48 by gabrlee           #+#    #+#             */
+/*   Updated: 2026/05/28 15:51:27 by gabrlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_print_reverse_alphabet(void){
-	//declare variables
-	char c = 'z';
-	while (c >= 'a'){
-	write(1,&c,1);
-	c --;
-}
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c --;
+	}
 }
--- C00/ex02/main.c
+++ C00/ex02/main.c
@@ -1,7 +1,21 @@
-#include <unistd.h>
-void ft_print_reverse_alphabet(void);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 15:47:03 by gabrlee           #+#    #+#             */
+/*   Updated: 2026/05/28 15:48:20 by gabrlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(void){
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+int	main(void)
+{
 	ft_print_reverse_alphabet();
-	return 0;
+	return (0);
 }
