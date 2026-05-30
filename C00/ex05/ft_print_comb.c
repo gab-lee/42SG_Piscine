a8ffd1d1b7859c110a70af437632ec22637c0c2e
Gabriel Lee <gabrlee@c2r1s9.42singapore.sg>
2026-05-30 10:22:22 +0800

    completed ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,38 +6,56 @@
 /*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 16:13:37 by gabrlee           #+#    #+#             */
-/*   Updated: 2026/05/28 16:37:57 by gabrlee          ###   ########.fr       */
+/*   Updated: 2026/05/30 10:21:10 by gabrlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_isnbrvalid(int *check, char h_place, char t_place, char o_place)
+{
+	if (h_place == t_place || h_place == o_place || t_place == o_place)
+	{
+		*check = 0;
+	}
+	if (h_place > t_place || h_place > o_place || t_place > o_place)
+	{
+		*check = 0;
+	}
+}
+
+void	ft_displaynbr(char h_place, char t_place, char o_place)
+{
+	write(1, &h_place, 1);
+	write(1, &t_place, 1);
+	write(1, &o_place, 1);
+	write(1, ",", 1);
+}
+
 void	ft_print_comb(void)
 {
-	int	n;
-	char	h_place; 
-	char	t_place; 
+	int		n;
+	int		check;
+	char	h_place;
+	char	t_place;
 	char	o_place;
 
 	n = 0;
 	while (n <= 999)
-	{ 
+	{
 		h_place = (n / 100) + '0';
-		t_place = ((n / 10)%10) + '0';
-		o_place = (n % 10) +'0';
-		if (h_place == t_place || h_place == o_place ||\
-		t_place == o_place || h_place > t_place ||\
-		h_place > o_place || t_place > o_place)
+		t_place = ((n / 10) % 10) + '0';
+		o_place = (n % 10) + '0';
+		check = 1;
+		ft_isnbrvalid(&check, h_place, t_place, o_place);
+		if (check)
 		{
+			ft_displaynbr(h_place, t_place, o_place);
 			n++;
 		}
 		else
-		{	
-			write(1, &h_place, 1);
-			write(1, &t_place, 1);
-			write(1, &o_place, 1);
-			write(1, ",", 1);
+		{
 			n++;
 		}
 	}
-}	
+}
