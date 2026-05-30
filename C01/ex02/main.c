172a00a4257b3621cf35b33a8b001ad7e41561d4
Gabriel Lee <gabrlee@c2r1s9.42singapore.sg>
2026-05-30 14:04:59 +0800

    update to C01

--- C01/ex02/main.c
+++ C01/ex02/main.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/30 10:44:43 by gabrlee           #+#    #+#             */
+/*   Updated: 2026/05/30 10:44:54 by gabrlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void 	ft_swap(int *a, int*b);
 
 int	main(void)
 {
-	int	a;
-	int	b;
+	int		a;
+	int		b;
 	char	c;
+
 	a = 1;
 	b = 2;
-	
-	ft_swap(&a,&b);
+	ft_swap(&a, &b);
 	c = a + '0';
-	write (1,"a",1);
-	write (1,&c,1);
+	write (1, "a", 1);
+	write (1, &c, 1);
 	c = b + '0';
-	write (1,"b",1);
-	write (1,&c,1);
+	write (1, "b", 1);
+	write (1, &c, 1);
 	return (0);
 }
