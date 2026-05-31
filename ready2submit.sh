d8890f3bbca7c6e8850aa8815406f43921225303
Gab <53164692+gab-lee@users.noreply.github.com>
2026-05-31 20:45:21 +0800

    basic scrip to copy files

---
+++ ready2submit.sh
@@ -0,0 +1,5 @@
+cp -r Shell01 ../Shell01
+cd ../Shell01
+mv gitignore .gitignore 
+git init 
+git remote add git@vogsphere.42singapore.sg:vogsphere/intra-uuid-16c43dac-44a3-4bb1-827a-541007e791ed-7446034-gabrlee
