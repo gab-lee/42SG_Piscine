8ee5e3a89d52aabc3f219ffc4a4fc285085aa83c
Gabriel Lee <gabrlee@c2r1s9.42singapore.sg>
2026-06-01 16:28:07 +0800

    completed ex08

---
+++ C02/ex08/ft_strlowcase.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 12:28:59 by gabrlee           #+#    #+#             */
+/*   Updated: 2026/06/01 14:09:20 by gabrlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_char_low_uppercase(char *c)
+{
+	if (*c >= 'A' && *c <= 'Z')
+	{
+		*c = *c +  32;
+	}
+}
+
+int	ft_strlowcase(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_char_low_uppercase(str);
+		str++;
+	}
+	return (1);
+}
---
+++ C02/ex08/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 12:26:43 by gabrlee           #+#    #+#             */
+/*   Updated: 2026/06/01 14:08:18 by gabrlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlowcase(char *str);
+
+int	main(void)
+{
+	char str[]="aABCVd!!!efg";
+	
+	ft_strlowcase(str);
+	printf("%s\n",str);
+	return (0);
+}
