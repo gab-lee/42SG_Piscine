5895f45a91dc23165cd87d92f42ca502802f77f2
Gabriel Lee <gabrlee@c2r1s9.42singapore.sg>
2026-06-01 16:27:52 +0800

    completed ex07

---
+++ C02/ex07/ft_struppcase.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_struppcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 12:28:59 by gabrlee           #+#    #+#             */
+/*   Updated: 2026/06/01 13:45:19 by gabrlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_char_up_lowercase(char *c)
+{
+	if (*c >= 'a' && *c <= 'z')
+	{
+		*c = *c -  32;
+	}
+}
+
+int	ft_strupcase(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_char_up_lowercase(str);
+		str++;
+	}
+	return (1);
+}
---
+++ C02/ex07/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 12:26:43 by gabrlee           #+#    #+#             */
+/*   Updated: 2026/06/01 13:47:21 by gabrlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strupcase(char *str);
+
+int	main(void)
+{
+	char str[]="abcd!!!efg";
+	
+	ft_strupcase(str);
+	printf("%s\n",str);
+	return (0);
+}
