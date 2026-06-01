cf9abd38b8b9c8ddbd5a7c2a66fd5ce503c0114e
Gabriel Lee <gabrlee@c2r1s9.42singapore.sg>
2026-06-01 09:33:20 +0800

    updated ready2submit

--- ready2submit.sh
+++ ready2submit.sh
@@ -1,5 +1,5 @@
-cp -r Shell01 ../Shell01
-cd ../Shell01
+cp -r C00 ../C00
+cd ../C00
 mv gitignore .gitignore 
 git init 
-git remote add git@vogsphere.42singapore.sg:vogsphere/intra-uuid-16c43dac-44a3-4bb1-827a-541007e791ed-7446034-gabrlee
+git remote add origin git@vogsphere.42singapore.sg:vogsphere/intra-uuid-12ac5309-c8ab-4389-b520-f2a4240a3d53-7446843-gabrlee
