e9972823dc7d47fd4d7d247642b3552353139850
Gabriel Lee <gabrlee@c2r3s3.42singapore.sg>
2026-06-08 13:42:32 +0800

    completed C03

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,38 +6,35 @@
 /*   By: gabrlee <gabrlee@student.42singapore.sg>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 15:39:33 by gabrlee           #+#    #+#             */
-/*   Updated: 2026/06/03 17:11:33 by gabrlee          ###   ########.fr       */
+/*   Updated: 2026/06/08 13:42:00 by gabrlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	len;
-	unsigned int 	i;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
 
-	len = 0;
+	dest_len = 0;
+	src_len = 0;
 	i = 0;
+	while (dest && dest[dest_len] != '\0')
+		dest_len++;
+	while (src && src[src_len] != '\0')
+		src_len++;
 	if (size == 0)
-		return (len);
+		return (dest_len + src_len);
+	else if (size <= dest_len)
+		return (size + src_len);
 	else
-		while (dest[len] != '\0' && len < size)
-			len++;
-	if (size >= len)
 	{
-		while (src[i] != '\0' && len < (size - 1))
+		while (src[i] != '\0' && i < (size - dest_len - 1))
 		{
-			dest[len] = src[i];
-			len++;
+			dest[dest_len + i] = src[i];
 			i++;
 		}
-		dest[len] = '\0';
+		dest[dest_len + i] = '\0';
+		return (dest_len + src_len);
 	}
-	else 
-		len = size;
-	while (src[i] != '\0' && len < size)
-		{
-			len++;
-			i++;
-		}
-	return (len);
 }
