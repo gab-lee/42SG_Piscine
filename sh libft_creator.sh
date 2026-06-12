b5d02a59d443e2e52cee26b6e1673159c23cd835
Gabriel Lee <53164692+gab-lee@users.noreply.github.com>
2026-06-12 14:50:05 +0800

    Create sh libft_creator.sh

---
+++ sh libft_creator.sh
@@ -0,0 +1,6 @@
+gcc -Wall -Wextra -Werror -c ft_putchar.c -o ft_putchar.o
+gcc -Wall -Wextra -Werror -c ft_swap.c -o ft_swap.o
+gcc -Wall -Wextra -Werror -c ft_putstr.c -o ft_putstr.o
+gcc -Wall -Wextra -Werror -c ft_strlen.c -o ft_strlen.o
+gcc -Wall -Wextra -Werror -c ft_strcmp.c -o ft_strcmp.o
+arc rcs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
