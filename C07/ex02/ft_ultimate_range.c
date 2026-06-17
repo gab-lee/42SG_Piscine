1b0a02d15a3cd7277c94bdc5f9b036ea312daa05
Gabriel Lee <gabrlee@c2r3s5.42singapore.sg>
2026-06-17 14:43:48 +0800

    updated C07

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -12,33 +12,22 @@
 
 #include <stdlib.h>
 
-void	ft_fill_array(int **array, int min, int max, int *size);
-
 int	ft_ultimate_range(int **range, int min, int max)
 {
-	int	size;
-
-	size = 0;
-	if (min >= max)
-	{
-		*range = malloc(1);
-		range[0] = NULL;
-		return (-1);
-	}
-	*range = malloc((max - min + 2) * sizeof(int));
-	if (!range)
-		return (-1);
-	range[max - min + 2] = NULL;
-	ft_fill_array(*range, min, max, &size);
-	return (size);
-}
-
-void	ft_fill_array(int *array, int min, int max, int *size)
-{
+	int	*tab;
 	int	i;
 
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	tab = malloc(sizeof(int) * (max - min));
+	if (!tab)
+		return (-1);
 	i = -1;
-	while (++i, (min + i) < max)
-		(*array)[i] = min + i;
-	*size = i;
+	while (++i, i < (max - min))
+		tab[i] = min + i;
+	*range = tab;
+	return (max - min);
 }
